984ce9083f413d6739f75a3bb0dfb3a58de18337
Minh-Tri Pham <pmtri80@gmail.com>
2020-10-07 13:43:26 +0100

    v0.0.6

--- mt/gpu/__init__.py
+++ mt/gpu/__init__.py
@@ -3,24 +3,24 @@ from __future__ import absolute_import, division, print_function
 from .arch import detect_machine
 
 
-__all__ = ['detect_machine', 'get_gpu_info']
+__all__ = ['detect_machine', 'get_mem_info']
 
 
-def get_gpu_info():
-    '''Returns a dictionary containing information about detected GPUs, or None if the architecture is unknown.'''
+def get_mem_info():
+    '''Returns a dictionary containing information about detected CPU/GPU devices and their memory usage, or None if the architecture is unknown.'''
     arch = detect_machine()
 
     if arch == 'amd64-cpu':
-        from .arch_amd64_cpu import get_gpu_info_impl
-        return get_gpu_info_impl()
+        from .arch_amd64_cpu import get_mem_info_impl
+        return get_mem_info_impl()
 
     if arch == 'amd64-nvidia':
-        from .arch_amd64_nvidia import get_gpu_info_impl
-        return get_gpu_info_impl()
+        from .arch_amd64_nvidia import get_mem_info_impl
+        return get_mem_info_impl()
     
     if arch == 'amd64-amd':
-        from .arch_amd64_amd import get_gpu_info_impl
-        return get_gpu_info_impl()
+        from .arch_amd64_amd import get_mem_info_impl
+        return get_mem_info_impl()
     
     if arch == 'unknown':
         return None
