f1ed9d4a70cb6f4af93cd756a39f0a1282b57535
Minh-Tri Pham <pmtri80@gmail.com>
2026-06-09 09:30:43 +0000

    updated version.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
 setup(
     name="mtgpu",
     description="Minh-Tri Pham's approach to detect gpu capacity",
-    author=["Minh-Tri Pham"],
+    author="Minh-Tri Pham",
     packages=find_namespace_packages(include=["mt.*"]),
     scripts=[
         "scripts/detect_machine",
@@ -24,13 +24,13 @@ setup(
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.gpu/mt.gpu.html",
         "Source Code": "https://github.com/inteplus/mtgpu",
     },
-    setup_requires=["setuptools-git-versioning<2"],
+    setup_requires=["setuptools-git-versioning>=3,<4"],
     setuptools_git_versioning={
         "enabled": True,
         "version_file": VERSION_FILE,
         "count_commits_from_version_file": True,
         "template": "{tag}",
-        "dev_template": "{tag}.dev{ccount}+{branch}",
-        "dirty_template": "{tag}.post{ccount}",
+        "dev_template": "{tag}",
+        "dirty_template": "{tag}",
     },
 )
